5afb9a7016a4fdc4f58caf8e20b3f43fface7fef
TheKnorx <the.knorx@proton.me>
2026-07-26 23:03:10 +0200

    Added ELF check and return value struct

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <elf.h>
+#include <iso646.h>
+#include <string.h>
 
 #include "common.h"
 
@@ -9,16 +11,20 @@
 #define ELF_HEADER_SIZE (sizeof(Elf64_Ehdr))
 #define NOP __asm__("NOP")  // No-Operation - assembly instruction
 
+typedef struct elf_info_table_S {
+    Elf64_Ehdr elf_header;
+    Elf64_Phdr prog_header;
+} elf_info_table_T;
 
 
 /**
  * Function for opening and gathering information from the ELF binary, like checking its properties or
  * extracting header- and general information
  *
+ * @param bin_infos Pointer to data field for storing the return values of this function
  * @param filename Path to and Filename of the ELF binary
- * @return I dont know yet
  */
-void* open_and_init_elf(const char* filename) {
+void open_and_parse_elf(elf_info_table_T* bin_infos, const char* filename) {
     DEBUG("Parsing elf file");
 
     // create a stdio file obj to the elf binary
@@ -30,19 +36,26 @@ void* open_and_init_elf(const char* filename) {
     if (NULL == elf_header) PRINT_ERROR("Failed to allocate space for the header-buffer");
     fread(elf_header, 1, ELF_HEADER_SIZE, elf_file_stream);
 
-    NOP; NOP; NOP;
+    // check if it's really an ELF binary
+    if (not memcmp(ELFMAG, elf_header->e_ident, SELFMAG)) goto ret;
 
-    return (void*)0xdeadbeef;
+    NOP; NOP; NOP;
+    ret:
+    free(elf_header);
+    fclose(elf_file_stream);
 }
 
 /**
- * Main function to start the loading and executing of the ELF binary
+ * Main funcstion to start the loading and executing of the ELF binary
  *
+ * @param argc
  * @param argv [1] The path and file name to the ELF binary
  */
 int main(const int argc, char **argv) {
     DEBUG("Entering main");
     if (argc < 2) THROW_CUSTOM_ERROR("Usage: %s <path/to/ELF/binary>", *argv);
 
-    open_and_init_elf(argv[1]);
+    elf_info_table_T binary_infos = {};
+    open_and_parse_elf(&binary_infos, argv[1]);
+
 }
