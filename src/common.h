9ba56ce5ab7cecb6f2dfe9b18f5e9c9a59addf13
TheKnorx <the.knorx@proton.me>
2026-07-26 22:30:53 +0200

    Begin of development of ELF file parser

--- src/common.h
+++ src/common.h
@@ -5,9 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// As a variadic argument we except format specifier for printf
 #define THROW_CUSTOM_ERROR(FORMAT_STR, ...) do { \
-    printf(FORMAT_STR, __VA_ARGS__);  \
-    return EXIT_FAILURE;  \
+    printf(FORMAT_STR, ##__VA_ARGS__);  \
+    exit(EXIT_FAILURE);  \
 } while (0);  // we should never get to this line
+
+// As the variadic argument we except an error code - can be left out
+#define PRINT_ERROR(INFO_STR, ...) do {  \
+    perror(INFO_STR);  \
+    exit(__VA_OPT__(+ 1 +) EXIT_FAILURE);  \
+} while (0);  // we should never get to this line
+
+#define DEBUG(FORMAT_STR, ...) {printf(FORMAT_STR, ##__VA_ARGS__); putchar('\n');}
+
+
 
 #endif //ELF_LOADER_COMMON_H
--- src/main.c
+++ src/main.c
@@ -1,14 +1,48 @@
+// Includes
 #include <stdio.h>
 #include <stdlib.h>
+#include <elf.h>
 
 #include "common.h"
 
+// Macros
+#define ELF_HEADER_SIZE (sizeof(Elf64_Ehdr))
+#define NOP __asm__("NOP")  // No-Operation - assembly instruction
+
+
+
+/**
+ * Function for opening and gathering information from the ELF binary, like checking its properties or
+ * extracting header- and general information
+ *
+ * @param filename Path to and Filename of the ELF binary
+ * @return I dont know yet
+ */
+void* open_and_init_elf(const char* filename) {
+    DEBUG("Parsing elf file");
+
+    // create a stdio file obj to the elf binary
+    FILE* elf_file_stream = fopen(filename, "r");
+    if (NULL == elf_file_stream) PRINT_ERROR("Failed to open the ELF binary");
+
+    // get the elf file header
+    Elf64_Ehdr* elf_header = calloc(1, ELF_HEADER_SIZE);
+    if (NULL == elf_header) PRINT_ERROR("Failed to allocate space for the header-buffer");
+    fread(elf_header, 1, ELF_HEADER_SIZE, elf_file_stream);
+
+    NOP; NOP; NOP;
+
+    return (void*)0xdeadbeef;
+}
+
 /**
  * Main function to start the loading and executing of the ELF binary
+ *
  * @param argv [1] The path and file name to the ELF binary
  */
 int main(const int argc, char **argv) {
+    DEBUG("Entering main");
     if (argc < 2) THROW_CUSTOM_ERROR("Usage: %s <path/to/ELF/binary>", *argv);
 
-    // ...
+    open_and_init_elf(argv[1]);
 }
