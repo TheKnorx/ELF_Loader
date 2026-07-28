eff4dd639a4d51608033d6caad9d150acd22886b
TheKnorx <the.knorx@proton.me>
2026-07-28 11:32:05 +0200

    Finished base structure of `open_and_parse_elf` function

--- src/common.h
+++ src/common.h
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SAFE_ERRONO int _errno_saved = errno;
+#define UNSAFE_ERRNO errno = _errno_saved;
+
 // As a variadic argument we except format specifier for printf
 #define THROW_CUSTOM_ERROR(FORMAT_STR, ...) do { \
     printf(FORMAT_STR, ##__VA_ARGS__);  \
@@ -12,10 +15,17 @@
 } while (0);  // we should never get to this line
 
 // As the variadic argument we except an error code - can be left out
-#define PRINT_ERROR(INFO_STR, ...) do {  \
+#define THROW_ERROR(INFO_STR, ...) do {  \
     perror(INFO_STR);  \
     exit(__VA_OPT__(+ 1 +) EXIT_FAILURE);  \
 } while (0);  // we should never get to this line
+
+// As a variadic argument we except format specifier for printf
+#define PRINT_CUSTOM_ERROR(FORMAT_STR, ...) do { \
+    SAFE_ERRONO  \
+    printf(FORMAT_STR, ##__VA_ARGS__);  \
+    UNSAFE_ERRNO  \
+} while (0);
 
 #define DEBUG(FORMAT_STR, ...) {printf(FORMAT_STR, ##__VA_ARGS__); putchar('\n');}
 
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <elf.h>
+#include <errno.h>
 #include <iso646.h>
 #include <string.h>
 
@@ -10,10 +11,11 @@
 // Macros
 #define ELF_HEADER_SIZE (sizeof(Elf64_Ehdr))
 #define NOP __asm__("NOP")  // No-Operation - assembly instruction
+#define GET_PHDR_ENTRY(_e_phoff, _e_phentsize) (_e_phoff + i * _e_phentsize)
 
 typedef struct elf_info_table_S {
-    Elf64_Ehdr elf_header;
-    Elf64_Phdr prog_header;
+    Elf64_Ehdr* elf_header;
+    Elf64_Phdr* prog_header_table;
 } elf_info_table_T;
 
 
@@ -23,30 +25,63 @@ typedef struct elf_info_table_S {
  *
  * @param bin_infos Pointer to data field for storing the return values of this function
  * @param filename Path to and Filename of the ELF binary
+ * @return Returns 0 if successful, an errno code if not successful
  */
-void open_and_parse_elf(elf_info_table_T* bin_infos, const char* filename) {
+int open_and_parse_elf(elf_info_table_T* bin_infos, const char* filename) {
     DEBUG("Parsing elf file");
+    int retval = -ENOEXEC;
+    errno = retval;
 
     // create a stdio file obj to the elf binary
     FILE* elf_file_stream = fopen(filename, "r");
-    if (NULL == elf_file_stream) PRINT_ERROR("Failed to open the ELF binary");
+    if (NULL == elf_file_stream) THROW_ERROR("Failed to open the ELF binary");
+    fseek(elf_file_stream, 0L, SEEK_END);
+    const long file_size = ftell(elf_file_stream);
+    rewind(elf_file_stream);
 
     // get the elf file header
     Elf64_Ehdr* elf_header = calloc(1, ELF_HEADER_SIZE);
-    if (NULL == elf_header) PRINT_ERROR("Failed to allocate space for the header-buffer");
-    fread(elf_header, 1, ELF_HEADER_SIZE, elf_file_stream);
+    if (NULL == elf_header) {
+        PRINT_CUSTOM_ERROR("Failed to allocate space for the header-buffer");
+        goto ret;
+    }
+    bin_infos->elf_header = elf_header;
+    if (ELF_HEADER_SIZE != fread(elf_header, 1, ELF_HEADER_SIZE, elf_file_stream)) {
+        PRINT_CUSTOM_ERROR("Failed to read the elf header");
+        goto ret;
+    }
 
-    // check if it's really an ELF binary
-    if (not memcmp(ELFMAG, elf_header->e_ident, SELFMAG)) goto ret;
+    /* Do consistency-checks to make sure it's really a valid ELF file */
+    // check the ELF magic number
+    if (0 != memcmp(ELFMAG, elf_header->e_ident, SELFMAG)) goto ret;
+    if (!elf_header->e_phoff) {
+        PRINT_CUSTOM_ERROR("Program header table size if 0");
+        goto ret;
+    }
+    if (elf_header->e_phoff < file_size) {
+        PRINT_CUSTOM_ERROR("Program header table is beyond EOF");
+        goto ret;
+    }
 
-    NOP; NOP; NOP;
-    ret:
-    free(elf_header);
+    // next get the program header table
+    fseeko(elf_file_stream, elf_header->e_phoff, SEEK_SET);
+    const Elf64_Word phdrtsize = elf_header->e_phentsize * elf_header->e_phnum;
+    Elf64_Phdr* phdrtable = calloc(1, phdrtsize);  // program header table
+    bin_infos->prog_header_table = phdrtable;
+    if (phdrtsize != fread(phdrtable, 1, phdrtsize, elf_file_stream)) {
+        PRINT_CUSTOM_ERROR("Failed to read the program header table");
+        goto ret;
+    }
+
+    return 0;  // we assume if we came here everything is good
+    ret:  // if we jumped here, something went wrong :(
     fclose(elf_file_stream);
+    if (-errno == retval) return retval;  // if the errno code is still the same from the beginning, return it
+    return -errno;  // else we just return the error code
 }
 
 /**
- * Main funcstion to start the loading and executing of the ELF binary
+ * Main function to start the loading and executing of the ELF binary
  *
  * @param argc
  * @param argv [1] The path and file name to the ELF binary
@@ -56,6 +91,6 @@ int main(const int argc, char **argv) {
     if (argc < 2) THROW_CUSTOM_ERROR("Usage: %s <path/to/ELF/binary>", *argv);
 
     elf_info_table_T binary_infos = {};
-    open_and_parse_elf(&binary_infos, argv[1]);
+    if (open_and_parse_elf(&binary_infos, argv[1]) < 0) THROW_ERROR("Failed to get ELF header information")
 
 }
