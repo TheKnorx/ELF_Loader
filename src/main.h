3e5812e23064bb411d3268e0885fbd4a0a7a8d9b
TheKnorx <the.knorx@proton.me>
2026-08-24 20:24:04 +0200

    Fixed wrong calculation for aligning page upward

--- src/main.h
+++ src/main.h
@@ -71,7 +71,7 @@ extern ssize_t memcpy_n(void* dest, const void* src);
 
 /* Align an address to a given page up the address range or down */
 #define ALIGN_TO_PAGE_DOWN(ADDR, PAGESIZE) ((ADDR) - (ADDR) % (PAGESIZE))
-#define ALIGN_TO_PAGE_UP(ADDR, PAGESIZE) ((ADDR) + (ADDR) % (PAGESIZE))
+#define ALIGN_TO_PAGE_UP(ADDR, PAGESIZE) ((ADDR) + ((PAGESIZE) - (ADDR) % (PAGESIZE)))
 
 /* This is the standard start of every function - it resets errno
  */
